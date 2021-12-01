d73f8acb6233b18eec61b33afc85712b6f03f0f2
neffel10 <60677114+neffel10@users.noreply.github.com>
2021-11-30 22:34:17 -0700

    1st

---
+++ eliminar.php
@@ -0,0 +1,24 @@
+<?php
+include("./inc/settings.php");
+validar();
+
+$id=$_POST['colum1'];
+
+$query="DELETE FROM table1 WHERE column1=$id;";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+if ( $conn->query($query)){
+    header("location:crud.php");
+}else{
+    echo "Algo salio mal <a href='https://localhost/crud/crud.php'> clic aqui para volver al crud</a>" ;
+
+}
+
+?>
---
+++ insertar.php
@@ -0,0 +1,29 @@
+<?php
+include("./inc/settings.php");
+validar();
+
+$identificador=$_POST ['identificador'];
+$nombre=$_POST ['nombre'];
+$fecha=$_POST ['fecha'];
+$numero=$_POST ['numero'];
+$numdouble=$_POST ['numdouble'];
+
+$query="INSERT INTO table1 (column1, column2, column3, column4, column5) VALUES ($identificador, '$nombre', '$fecha', $numero, $numdouble);";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+if ( $conn->query($query)){
+    header("location:crud.php");
+}else{
+    echo "Algo salio mal <a href='https://localhost/crud/crud.php'> clic aqui para volver al crud</a>" ;
+
+}
+
+
+?>
